b3f04716a1f8347b3836e0d5a51a26d85bae913f
chotinun <chotinun@chotinuns-MacBook-Pro.local>
2023-09-20 22:40:30 +0700

    first commit

---
+++ receiver.js
@@ -0,0 +1,16 @@
+const amqp = require('amqplib/callback_api');
+
+const host = 'localhost:5672'
+const credential = 'guest:guest'
+const server = `amqp://${credential}@${host}`;
+
+amqp.connect(server, (err, conn) => {
+  conn.createChannel((err, ch) => {
+    const queue = 'hello';
+    ch.assertQueue(queue, {durable: false});
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+    ch.consume(queue, function(msg) {
+      console.log(" [x] Received %s", msg.content.toString());
+    }, {noAck: true});
+  });
+});
---
+++ sender.js
@@ -0,0 +1,16 @@
+const amqp = require('amqplib/callback_api');
+
+const host = 'localhost:5672'
+const credential = 'guest:guest'
+const server = `amqp://${credential}@${host}`;
+
+amqp.connect(server, (err, conn) => {
+  conn.createChannel((err, ch) => {
+    const queue = 'hello';
+    ch.assertQueue(queue, {durable: false});
+    // Note: on Node 6 Buffer.from(msg) should be used
+    ch.sendToQueue(queue, new Buffer('Hello World!'));
+    console.log(" [x] Sent 'Hello World!'");
+    setTimeout(() => { conn.close(); process.exit(0) }, 500);
+  });
+});
